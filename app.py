6512b0c190e3477c680fd0e76f1bcf4196a104c5
zalzah00 <zalzah00@gmail.com>
2025-11-12 11:19:15 +0000

    fds

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return(render_template("index.html"))
 
+@app.route("/login",methods=["GET","POST"])
+def main():
+    return(render_template("main.html"))
+
 if __name__ == "__main__":
     app.run()
