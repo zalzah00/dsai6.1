73075b4005758c0c616e29c0ead2c8e774f6781c
zalzah00 <zalzah00@gmail.com>
2025-11-12 12:22:40 +0000

    wf

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from flask import Flask, render_template, request
 import joblib
+import os
+from groq import Groq
+
+#os.environ["GROQ_API_KEY"] = ""
+client = Groq()
 
 app = Flask(__name__)
 
@@ -15,12 +20,32 @@ def main():
 def dbs():
     return(render_template("dbs.html"))
 
+
 @app.route("/dbs_prediction",methods=["GET","POST"])
 def dbs_prediction():
     q = float(request.form.get("q"))
     model = joblib.load("DBS_SGD_model.pkl")
-    r=model.predict([[q]])
+    r = model.predict([[q]])
     return(render_template("dbs_prediction.html",r=r))
+
+@app.route("/chatbot",methods=["GET","POST"])
+def chatbot():
+    return(render_template("chatbot.html"))
+
+@app.route("/llama",methods=["GET","POST"])
+def llama():
+    return(render_template("llama.html"))
+
+@app.route("/llama_result",methods=["GET","POST"])
+def llama_result():
+    q = request.form.get("q")
+    r = client.chat.completions.create(
+    model="llama-3.1-8b-instant",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": q}])
+    r=r.choices[0].message.content
+    return(render_template("llama_result.html",r=r))
 
 if __name__ == "__main__":
     app.run()
