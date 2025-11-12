5ef89366f8df572159cf2eabff5ace77edfee425
zalzah00 <zalzah00@gmail.com>
2025-11-12 11:36:51 +0000

    fewafg

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def index():
     return(render_template("index.html"))
 
-@app.route("/login",methods=["GET","POST"])
+@app.route("/main",methods=["GET","POST"])
 def main():
     return(render_template("main.html"))
 
